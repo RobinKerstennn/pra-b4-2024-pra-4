676744a605e108755c37912e46b8c3e3e10e043e
martwijnman <153086626+martwijnman@users.noreply.github.com>
2024-05-27 18:51:00 +0200

    Making OPP classes

--- PRA_B4_FOTOKIOSK/models/KioskProduct.cs
+++ PRA_B4_FOTOKIOSK/models/KioskProduct.cs
@@ -11,5 +11,23 @@ namespace PRA_B4_FOTOKIOSK.models
 
         public string Name { get; set; }
 
+        public double Price { get; set; }
+
+        public string Description { get; set; }
+
+        public void ShowLoop()
+        {
+            //foreach (KioskProduct product in ShopManager.Products)
+            //{
+                
+            //}
+        }
+
+    }
+
+    public class OrderdProduct()
+    {
+        private int PhotoNumber;
+        private string 
     }
 }
