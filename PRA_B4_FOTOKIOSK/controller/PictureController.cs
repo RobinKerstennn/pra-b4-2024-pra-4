33020cf3fd7990153c91694575006b8401bfa392
ericokwabi <deo0006@edu.curio.nl>
2024-06-03 12:13:36 +0200

    photos van tussen 2 minuten geleden en 30 minuten geleden

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -7,21 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
-
 namespace PRA_B4_FOTOKIOSK.controller
-{ 
-
+{
     public class PictureController
     {
         // De window die we laten zien op het scherm
         public static Home Window { get; set; }
 
-
         // De lijst met fotos die we laten zien
         public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
-
 
         // Start methode die wordt aangeroepen wanneer de foto pagina opent.
         public void Start()
@@ -29,130 +23,196 @@ namespace PRA_B4_FOTOKIOSK.controller
             var now = DateTime.Now;
             int day = (int)now.DayOfWeek;
 
+            // Calculate the time range
             DateTime lowerBound = now.AddMinutes(-30);
             DateTime upperBound = now.AddMinutes(-2);
+            var directoryPath = @"../../../fotos";
 
             // Initializeer de lijst met fotos
-            // WAARSCHUWING. ZONDER FILTER LAADT DIT ALLES!
-            // foreach is een for-loop die door een array loopt
-            foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
+            foreach (string dir in Directory.GetDirectories(directoryPath))
             {
-                /**
-                 * dir string is de map waar de fotos in staan. Bijvoorbeeld:
-                 * \fotos\0_Zondag
-                 */
                 var folderName = Path.GetFileName(dir);
                 var folderDayNumber = folderName.Split('_');
-                /*string fileName = Path.GetFileNameWithoutExtension(@"../../../fotos");
-                var parts = fileName.Split("_");*/
 
-
-
-                if (folderDayNumber.Length > 1)
+                if (folderDayNumber.Length > 1 && int.TryParse(folderDayNumber[0], out int dayNumber) && dayNumber == day)
                 {
-                    int dayNumber;
-                    if (int.TryParse(folderDayNumber[0], out dayNumber))
+                    List<KioskPhoto> photosForDay = new List<KioskPhoto>();
+
+                    foreach (string file in Directory.GetFiles(dir))
                     {
-                        if (dayNumber == day)
+                        string fileName = Path.GetFileNameWithoutExtension(file);
+                        var parts = fileName.Split('_');
+
+                        if (parts.Length >= 3 && int.TryParse(parts[0], out int hour) &&
+                            int.TryParse(parts[1], out int minute) &&
+                            int.TryParse(parts[2], out int seconds))
                         {
-                            foreach (string file in Directory.GetFiles(dir))
+                            DateTime photoTime = new DateTime(now.Year, now.Month, now.Day, hour, minute, seconds);
+
+                            if (photoTime > now)
                             {
-                                /**
-                                 * file string is de file van de foto. Bijvoorbeeld:
-                                 * \fotos\0_Zondag\10_05_30_id8824.jpg
-                                 */
-                                string fileName = Path.GetFileNameWithoutExtension(file);
-                                var parts = fileName.Split("_");
+                                photoTime = photoTime.AddDays(-1);
+                            }
 
-                                if (parts.Length > 0)
-                                {
-                                    if (int.TryParse(parts[0], out int hour) &&
-                                        int.TryParse(parts[1], out int minute) &&
-                                        int.TryParse(parts[2], out int seconds))
-                                    {
-                                        DateTime photoTime = new DateTime(now.Year, now.Month, now.Day, hour, minute,
-                                            seconds);
-
-                                        if (photoTime > now)
-                                        {
-                                            photoTime = photoTime.AddDays(-1);
-                                        }
-
-                                        if (photoTime >= lowerBound && photoTime <= upperBound)
-                                        {
-                                            PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
-                                        }
-                                    }
-                                  
-                                }
+                            if (photoTime >= lowerBound && photoTime <= upperBound)
+                            {
+                                PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
                             }
                         }
-                   
-                    {
-
                     }
                 }
-
             }
 
+            // Update de fotos
+            PictureManager.UpdatePictures(PicturesToDisplay);
         }
+
+        // Wordt uitgevoerd wanneer er op de Refresh knop is geklikt
         public void RefreshButtonClick()
         {
-                        
+            // You can add code here to refresh the photo display if needed
         }
     }
 }
 
-    //public class PictureController
-    //{
+
+
+
+
+
+/*using PRA_B4_FOTOKIOSK.magie;
+using PRA_B4_FOTOKIOSK.models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PRA_B4_FOTOKIOSK.controller
+{
+    public class PictureController
+    {
         // The window displayed on the screen
-      //  public static Home Window { get; set; }
+        public static Home Window { get; set; }
 
         // The list of photos to display
-      //  public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
+        public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
 
-        // Start method that is called when the photo page opens.
-        //public void Start()
-        //{
-          //  var now = DateTime.Now;
-          //  int day = (int)now.DayOfWeek;
-          //  DateTime lowerBound = now.AddMinutes(-30);
-          //  DateTime upperBound = now.AddMinutes(-2);
-          //  var directoryPath = @"../../../fotos";
+        // Start method that is called when the photo page opens
+        public void Start()
+        {
+            try
+            {
+                var now = DateTime.Now;
+                int day = (int)now.DayOfWeek;
 
-            // Initialize the list of photos
-            // WARNING. WITHOUT FILTER THIS LOADS EVERYTHING!
-          //  foreach (string dir in Directory.GetDirectories(directoryPath))
-          //  {
-               // var folderName = Path.GetFileName(dir);
-               // var folderDayNumber = folderName.Split('_');
+                DateTime lowerBound = now.AddMinutes(-30);
+                DateTime upperBound = now.AddMinutes(-2);
+                var directoryPath = @"../../../fotos";
 
-                //if (folderDayNumber.Length > 1 && int.TryParse(folderDayNumber[0], out int dayNumber) && dayNumber == day)
-              //  {
-            //        foreach (string file in Directory.GetFiles(dir))
-          //          {
-        //                var fileNameParts = Path.GetFileNameWithoutExtension(file).Split('_');
-      //                  if (fileNameParts.Length >= 3 && int.TryParse(fileNameParts[0], out int hour) &&
-    //                        int.TryParse(fileNameParts[1], out int minute) && int.TryParse(fileNameParts[2], out int second))
-  //                      {
-//                            DateTime photoTime = new DateTime(now.Year, now.Month, now.Day, hour, minute, second);
+                // Initialize the list of photos
+                foreach (string dir in Directory.GetDirectories(directoryPath))
+                {
+                    var folderName = Path.GetFileName(dir);
+                    var folderDayNumber = folderName.Split('_');
 
-      //                      if (photoTime > now)
-    //                        {
-  //                              photoTime = photoTime.AddDays(-1);
-//                            }
+                    if (folderDayNumber.Length > 1 && int.TryParse(folderDayNumber[0], out int dayNumber) && dayNumber == day)
+                    {
+                        List<KioskPhoto> photosForDay = new List<KioskPhoto>();
 
-//if (photoTime >= lowerBound && photoTime <= upperBound)
-//{
- //PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
-         //                  }
-       //              }
-     //          }
+                        foreach (string file in Directory.GetFiles(dir))
+                        {
+                            string fileName = Path.GetFileNameWithoutExtension(file);
+                            var parts = fileName.Split('_');
 
-// Executed when the Refresh button is clicked
-//public void RefreshButtonClick()
-//{
-//}
-    
+                            if (parts.Length >= 3 && int.TryParse(parts[0], out int hour) &&
+                                int.TryParse(parts[1], out int minute) &&
+                                int.TryParse(parts[2], out int seconds))
+                            {
+                                DateTime photoTime = new DateTime(now.Year, now.Month, now.Day, hour, minute, seconds);
 
-//}
+                                if (photoTime > now)
+                                {
+                                    photoTime = photoTime.AddDays(-1);
+                                }
+
+                                if (photoTime >= lowerBound && photoTime <= upperBound)
+                                {
+                                    photosForDay.Add(new KioskPhoto() { Id = 0, Source = file });
+                                }
+                            }
+                        }
+
+                        // Pair photos taken by the two cameras
+                        var pairedPhotos = PairPhotos(photosForDay);
+                        PicturesToDisplay.AddRange(pairedPhotos);
+                    }
+                }
+
+                // Update the photos
+                PictureManager.UpdatePictures(PicturesToDisplay);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in Start method: " + ex.Message);
+            }
+        }
+
+        // Pair photos taken by the two cameras 60 seconds apart
+        private List<KioskPhoto> PairPhotos(List<KioskPhoto> photos)
+        {
+            List<KioskPhoto> pairedPhotos = new List<KioskPhoto>();
+
+            var groupedPhotos = photos
+                .Select(photo => new
+                {
+                    Photo = photo,
+                    Time = ExtractDateTimeFromFileName(photo.Source)
+                })
+                .OrderBy(photo => photo.Time)
+                .ToList();
+
+            for (int i = 0; i < groupedPhotos.Count - 1; i++)
+            {
+                var currentPhoto = groupedPhotos[i];
+                var nextPhoto = groupedPhotos[i + 1];
+
+                if (nextPhoto.Time - currentPhoto.Time == TimeSpan.FromSeconds(60))
+                {
+                    pairedPhotos.Add(currentPhoto.Photo);
+                    pairedPhotos.Add(nextPhoto.Photo);
+                    i++; // Skip the next photo since it is paired with the current one
+                }
+                else
+                {
+                    pairedPhotos.Add(currentPhoto.Photo);
+                }
+            }
+
+            return pairedPhotos;
+        }
+
+        // Extract DateTime from the file name
+        private DateTime ExtractDateTimeFromFileName(string fileName)
+        {
+            var parts = Path.GetFileNameWithoutExtension(fileName).Split('_');
+
+            if (parts.Length >= 3 && int.TryParse(parts[0], out int hour) &&
+                int.TryParse(parts[1], out int minute) &&
+                int.TryParse(parts[2], out int seconds))
+            {
+                return new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, hour, minute, seconds);
+            }
+
+            return DateTime.MinValue; // Return minimum value if parsing fails
+        }
+
+        // Executed when the Refresh button is clicked
+        public void RefreshButtonClick()
+        {
+            // Clear the current list of pictures and reload
+            PicturesToDisplay.Clear();
+            Start();
+        }
+    }
+}
+*/
