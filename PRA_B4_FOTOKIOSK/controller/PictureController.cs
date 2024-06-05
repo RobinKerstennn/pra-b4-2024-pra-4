926dd06d83935fb3fe3e42358d5a79ac7d6ce6ea
martwijnman <153086626+martwijnman@users.noreply.github.com>
2024-06-05 08:43:25 +0200

    fixed refresh button

--- PRA_B4_FOTOKIOSK/controller/PictureController.cs
+++ PRA_B4_FOTOKIOSK/controller/PictureController.cs
@@ -4,7 +4,100 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+namespace PRA_B4_FOTOKIOSK.controller
+{
+    public class PictureController
+    {
+        // De window die we laten zien op het scherm
+        public static Home Window { get; set; }
 
+
+        // De lijst met fotos die we laten zien
+        public List<KioskPhoto> PicturesToDisplay = new List<KioskPhoto>();
+        
+        
+        // Start methode die wordt aangeroepen wanneer de foto pagina opent.
+        public void Start()
+        {
+            var now = DateTime.Now;
+            int day = (int)now.DayOfWeek;
+
+            DateTime lowerBound = now.AddMinutes(-30);
+            DateTime upperBound = now.AddMinutes(-2);
+
+            // Initializeer de lijst met fotos
+            // WAARSCHUWING. ZONDER FILTER LAADT DIT ALLES!
+            // foreach is een for-loop die door een array loopt
+            foreach (string dir in Directory.GetDirectories(@"../../../fotos"))
+            {
+                /**
+                 * dir string is de map waar de fotos in staan. Bijvoorbeeld:
+                 * \fotos\0_Zondag
+                 */
+                var folderName = Path.GetFileName(dir);
+                var folderDayNumber = folderName.Split('_');
+
+                /*string fileName = Path.GetFileNameWithoutExtension(@"../../../fotos");
+                var parts = fileName.Split("_");*/
+
+                
+                if (folderDayNumber.Length > 1)
+                {
+                    int dayNumber;
+                    if (int.TryParse(folderDayNumber[0], out dayNumber))
+                        if (dayNumber == day)
+                        {
+                            foreach (string file in Directory.GetFiles(dir))
+                            {
+                                /**
+                                 * file string is de file van de foto. Bijvoorbeeld:
+                                 * \fotos\0_Zondag\10_05_30_id8824.jpg
+                                 */
+                                string fileName = Path.GetFileNameWithoutExtension(file);
+                                var parts = fileName.Split("_");
+
+                                if (parts.Length > 0)
+                                {
+                                    if (int.TryParse(parts[0], out int hour) &&
+                                        int.TryParse(parts[1], out int minute) &&
+                                        int.TryParse(parts[2], out int seconds))
+                                    {
+                                        DateTime photoTime = new DateTime(now.Year, now.Month, now.Day, hour, minute, seconds);
+
+                                        if (photoTime > now)
+                                        {
+                                            photoTime = photoTime.AddDays(-1);
+                                        }
+
+                                        if (photoTime >= lowerBound && photoTime <= upperBound)
+                                        {
+                                            PicturesToDisplay.Add(new KioskPhoto() { Id = 0, Source = file });
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    {
+                       
+                    }
+                }
+                
+            }
+
+                // Update de fotos
+                PictureManager.UpdatePictures(PicturesToDisplay);
+        }
+
+        // Wordt uitgevoerd wanneer er op de Refresh knop is geklikt
+        public void RefreshButtonClick()
+        {
+            
+        }
+
+    }
+}
+
+/**
 namespace PRA_B4_FOTOKIOSK.controller
 {
     public class PictureController
@@ -54,10 +147,7 @@ namespace PRA_B4_FOTOKIOSK.controller
 
                             if (photoTime > now)
                             {
-                                /**
-                                 * file string is de file van de foto. Bijvoorbeeld:
-                                 * \fotos\0_Zondag\10_05_30_id8824.jpg
-                                 */
+                               
                                 string fileName = Path.GetFileNameWithoutExtension(file);
                                 var parts = fileName.Split("_");
 
@@ -123,4 +213,4 @@ namespace PRA_B4_FOTOKIOSK.controller
         }
     }
 }
-
+*/
